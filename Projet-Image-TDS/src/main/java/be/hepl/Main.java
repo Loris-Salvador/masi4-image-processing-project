c9d62de113db9c5403bb70c6bdeb86db109368f4
flo <flobaillien@gmail.com>
2025-04-13 12:28:37 +0200

    Photos

--- Projet-Image-TDS/src/main/java/be/hepl/Main.java
+++ Projet-Image-TDS/src/main/java/be/hepl/Main.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Exécution dans le thread EDT (Event Dispatch Thread)
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
